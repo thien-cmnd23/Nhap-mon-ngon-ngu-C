a100c8701ec52ce8e6fa36043024a7745ce16a59
thien-cmnd23 <72543503+thien-cmnd23@users.noreply.github.com>
2020-12-10 16:57:41 +0700

    Create hoadon.c

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	//khai bao bien
+	float dongia, soluong , thanhtien;
+    printf("nhap bien dongia cua 1 san pham duoc mua tu ban phim");
+    scanf("%c" ,&dongia);
+    printf("nhap bien soluong cua 1 san pham duoc mua tu ban phim");
+    scanf("%d",&soluong);
+    printf("nhap bien thanhtien giam 25% khi mua 5 san pham");
+    scanf("%d",&thanhtien);
+    
+   
+ 
+}
