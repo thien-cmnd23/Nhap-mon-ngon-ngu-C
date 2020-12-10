4a3104dd269e85c99026b3c012b6afa9dfcf6c14
thien-cmnd23 <72543503+thien-cmnd23@users.noreply.github.com>
2020-12-10 15:04:52 +0700

    Create amduong.c

---
+++ Bài tập 3/amduong.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    if(a > 0){
+       printf("%d duong", a);
+	}else if (a == 0) {
+	   printf("a = 0", a);
+	}else {
+	   printf("%d am", a);
+}
+	
+	
+    
+    getchar();
+    return 0;
+}
