628781e176daf930176f6563ba0f48e82d136b01
thien-cmnd23 <72543503+thien-cmnd23@users.noreply.github.com>
2020-12-10 15:42:33 +0700

    Create sosanh.c

---
+++ Bài tập 3/sosanh.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	//nhap hai so a b tu ban phim 
+	int a=5,b=6;
+	if(a > b){
+	printf("a>b",a,b);
+		}else if (a == b){printf("a=b",a,b);
+	}else {
+		printf("a<b",a,b);
+	}
+getchar;
+return 0;
+
+}
