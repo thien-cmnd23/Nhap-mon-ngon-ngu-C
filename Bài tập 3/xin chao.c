e7f0d7fc59f7d9f8c8fee26de25fa1674b46a120
thien-cmnd23 <72543503+thien-cmnd23@users.noreply.github.com>
2020-12-24 14:11:40 +0700

    Create xin chao.c

---
+++ Bài tập 3/xin chao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main(){
+
+	int count;
+	printf("t\ 10 dong xin chao\n");
+    for (count = 1; count <= 9; count++)
+	    printf("\n \n \t xin chao");
+	printf("\n\n\t xin chao.\n");
+}	
