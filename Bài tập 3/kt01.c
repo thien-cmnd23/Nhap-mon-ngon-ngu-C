091e469af51c564a7ae42ed1bedbd2f3e72068c6
thien-cmnd23 <72543503+thien-cmnd23@users.noreply.github.com>
2021-01-07 16:32:56 +0700

    Create kt01.c

---
+++ Bài tập 3/kt01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+   int a;
+   printf("bang cuu chuong \n\n\n");
+   printf("bang 2:\n 2x1= 2\n 2x2 =4\n 2x3 =6\n\n");
+   printf("bang 3:\n 3x1= 3\n 3x2 =6\n 3x3 =9\n\n");
+   printf("bang 4:\n 4x1= 4\n 4x2 =8\n 4x3 =12\n\n");
+
+return 0;
+}
